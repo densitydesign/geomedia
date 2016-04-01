d76c13d3c4dfdbebf0f7d77b2aef001142e07884
fenicento <stonecoldcrazy@gmail.com>
2016-04-01 18:14:12 +0200

    going on

--- app/scripts/controllers/time-media.js
+++ app/scripts/controllers/time-media.js
@@ -8,10 +8,35 @@
  * Controller of the geomediaApp
  */
 angular.module('geomediaApp')
-  .controller('TimeMediaCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('TimeMediaCtrl', function ($scope,$rootScope) {
+
+
+    $scope.computeAggregation = function() {
+      $scope.streams = d3.nest()
+        .key(function(d) { return d.media; })
+        .key(function(d) { return d.time; })
+        .key(function(d) { return d.TAG_event; })
+        .rollup(function(leaves) { return leaves.length; })
+        .entries($rootScope.bymedia.bottom(Infinity))
+    }
+
+    $scope.getIndex = function(k) {
+      return _.findIndex($scope.streams, function(d,i){return d.key == k})
+    }
+
+    if($rootScope.bytime) {
+      $scope.computeAggregation()
+    }
+
+
+    $rootScope.$watch('bytime',function(newValue,oldValue){
+      if (newValue && newValue != oldValue) {
+        $scope.computeAggregation()
+      }
+    })
+
+
+
+
+
   });
